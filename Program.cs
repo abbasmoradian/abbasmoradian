1225b86e5a91c9a5336dd08ddd36132f82a7fd3c
abbasmoradian <abbasmoradian0786@gmail.com>
2024-10-12 20:27:07 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,115 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Whats Your First Name?");
+StringName.Console.ReadLine() ?? "";
+Console.WriteLine(Whats Your Last Name?);
+StringName.Console.ReadLine() ?? "";
+Short age = ();
+Unit National Number = ();
+int Smaller Age = 15;
+int Bigger Age = 50;
+Long Bigger Age = int Smaller Age;
+int Smaller Age = (int)Long Bigger Age;
+int Age = int.Parse;
+String Age = AgeInt.ToString;
+StringAge.Console.ReadLine() ?? "";
+bool = (15<Age<120)True;
+if;
+{True = You are valid to Register;
+False = You are Not valid To Register};
+
+Console.WriteLine("please enter Your PhoneNumber");
+if(PhoneNumber.Length == 10 && !PhoneNumber.Startswith("0"));
+{
+    PhoneNumber =$"0{PhoneNumber}"
+}
+else;
+{
+    Console.WriteLine("please enter valid Phone Number")
+}
+
+if(PhoneNumber.Startswith("+98"));
+{
+    PhoneNumber = PhoneNumber.Replase("+98,0")
+}
+
+if(String.IsNullorEmpty(PhoneNumber));
+{
+    Console.WriteLine("Please enter valid Phone Number")
+}
+
+PhoneNumber = PhoneNumber.Trime();
+
+Console.WriteLine("Please enter your birthmonth");
+
+String.birthmonth.Console.ReadLine() ?? "";
+
+birthmonth = birthmonth.Trim().Tolower();
+
+
+///قسمت 1 تمرین در منزل :کاربر دو عدد را وارد نماید و خروجی آن را دریافت نماید
+///این خروجی در قالب متن میباشد
+Public static int Add(int bornyear,int Age);
+{
+Console.WriteLine("Please enter your bornyear and your Age")
+int Console.ReadLine()
+int result = bornyear + Age
+return result
+}
+///قسمت 2 تمرین در منزل:کاربر 3عدد را وارد کند و خروجی آن را دریافت کند
+Public static int Add(int Num1,int Num2,int Num3);
+{
+console.WriteLine("please enter Num1,Num2,Num3")
+int console.ReadLine()
+int result = Num1,Num2,Num3
+return result
+}
+///قسمت 3 تمرین در منزل:کاربر دو عدد را وارد کند و شما به کمک متد ها تشخیص دهید
+///ایا عدد بر هم بخش پذیر است یا نه
+console.WriteLine("Please enter your firstNumber");
+int.console.ReadLine();
+Console.WriteLine("please enter your secondNumber");
+int Console.ReadLine();
+int firstNumber÷secondNumber;
+bool = (secondNumber = 0)false;
+if;
+{ 
+false = Console.WriteLine("please dont use 0 for secondNumber")
+true = return
+}
+
+///قسمت4 تمرین در منزل:کاربر یک عدد را وارد کند وشما باید به وسیله ی یک متد 
+///تشخیص دهید عدد زوج است یا فرد این را در قالب یک پیام نشان دهید
+Console.WriteLine("Please enter your Number");
+int Console.ReadLine();
+if(Number(Endwith 1,3,5,7,9));
+{
+    Console.WriteLine("your Number is the odd Number")
+}
+else;
+{
+    Console.WriteLine("your Number is the even Number")
+}
+
+///قسمت 5 تمرین در منزل:کاربر دو عدد را وارد نماید و برسی کنید که کدام
+///کوچکتر است و عدد کوچکتر را باید برگرداند
+Console.WriteLine("please enter num1");
+int Console.ReadLine();
+Console.WriteLine("please enter num2");
+int Console.ReadLine();
+int MathMin(num1,num2);
+return MathMin;
+
+///قسمت 6 نمرین در منزل:کاربر 5 عدد را وارد نماید و باید برسی نمایید که کدام
+///کوچکتر است و عدد کوچکتر را باید برگرداند
+Console.WriteLine("please enter num1");
+Console.ReadLine();
+Console.WriteLine("please enter num2");
+Console.ReadLine()
+Console.WriteLine("please enter num3")
+Console.ReadLine()///ناقص
+///قسمت 7 تمرین در منزل :متدی برای دنباله های حسابی داشته باشید که کاربر
+///مشخص کند تا چند رقم باید پیش بروید
+
+
+///متدی برای دنباله فیبو ناچی داشته باشید
+
